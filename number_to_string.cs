22c37aa5f683d669cf08df1c3adfc6b00414354d
Mason <chtuff@gmail.com>
2024-09-26 11:37:36 -0400

    solution

---
+++ number_to_string.cs
@@ -0,0 +1,16 @@
+// RESULT:  transform a number into a string.
+// EXAMPLE: Examples (input --> output):
+//          123  --> "123"
+//PSEUDDOCODE: use the ToString method
+
+
+
+using System;
+
+public class Kata
+{
+  public static string NumberToString(int num)
+  {
+    return num.ToString();
+  }
+}
