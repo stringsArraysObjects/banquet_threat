2426abbda14d432859e1e4fd6f403abc1bbb1926
Mason <chtuff@gmail.com>
2024-09-30 12:00:08 -0400

    solution

---
+++ opposite.cs
@@ -0,0 +1,14 @@
+// RESULT:  given a number (integer / decimal / both depending on the language),
+//          find its opposite (additive inverse).
+// PSEUDOCODE: use the negative sign to get the additive inverse of a number 
+
+using System;
+
+public class Kata
+    {
+        public static int Opposite(int number)
+        {
+            // Happy Coding
+            return -number;
+        }
+    }
