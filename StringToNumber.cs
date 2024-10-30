78cb3bfc3022a7f703020320cf3499036bdb18fe
Mason <chtuff@gmail.com>
2024-10-30 18:10:10 -0400

    solution

---
+++ StringToNumber.cs
@@ -0,0 +1,10 @@
+// REUSLUT: Transform a string into a number.
+
+
+using System;
+  public class Kata
+  {
+    public static int StringToNumber(String str) {
+         return (int.Parse(str));
+  }
+}
