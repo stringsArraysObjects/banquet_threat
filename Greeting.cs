be4dc23d8350acd4b3c658544c66523e42871394
Chris <chtuff@gmail.com>
2024-11-24 13:38:55 -0500

    solution

---
+++ Greeting.cs
@@ -0,0 +1,9 @@
+// RESULT:  return a greeting statement that uses an input. 
+// EXAMPLE: "Hello, <name> how are you doing today?"
+
+using System;
+
+public static class Kata
+{
+  public static string Greet(string name) => $"Hello, {name} how are you doing today?";
+}
