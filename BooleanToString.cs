5d3ce7bb2e7392a1518e790d2978838448f4c1fb
Chris <chtuff@gmail.com>
2024-11-25 11:12:34 -0500

    solution

---
+++ BooleanToString.cs
@@ -0,0 +1,9 @@
+// RESULT:  Convert the given boolean value into its string representation
+using System;
+public class kata
+{
+  public static string BooleanToString(bool b)
+  {
+    return b.ToString();
+  }
+}
