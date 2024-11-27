e2db48f73d22a6f18ec618f204c0673a84259bb1
Chris <chtuff@gmail.com>
2024-11-26 19:33:05 -0500

    solution

---
+++ basicOp.cs
@@ -0,0 +1,19 @@
+// RESULT:  Create a function that does four basic mathematical operations.
+
+namespace Solution
+{
+  public static class Program
+  {
+    public static double basicOp(char op, double val1, double val2)
+    {
+      switch(op){
+        case '+': return val1+val2;
+        case '-': return val1-val2;
+        case '*': return val1*val2;
+        case '/': return val1/val2;
+        default:
+           throw new System.ArgumentException("Unknown operation!", op.ToString());
+      }
+    }
+  }
+}
