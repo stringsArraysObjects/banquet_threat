ad500c5a6d9b1deafe0b681190d09a041911c5e6
Chris <chtuff@gmail.com>
2024-11-27 22:02:17 -0500

    solution

---
+++ Hydrated.cs
@@ -0,0 +1,11 @@
+// RESULT:  Return the number of litres Nathan will drink, rounded to the smallest value.
+
+using System;
+
+public class Kata
+{
+  public static int Litres(double time)
+  {    
+    return (int)(time/2);
+  }
+}
