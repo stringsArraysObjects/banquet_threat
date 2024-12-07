182af97d221ca69eff93c7ccdec2f5a5731fd3a8
Chris <chtuff@gmail.com>
2024-12-06 20:08:22 -0500

    solution

---
+++ SumArray.cs
@@ -0,0 +1,11 @@
+// RESULT:  Takes an array of numbers and returns the sum of the numbers.
+
+using System.Linq;
+
+public class Kata
+{
+  public static double SumArray(double[] array)
+  {
+    return array.Sum();
+  }
+}
